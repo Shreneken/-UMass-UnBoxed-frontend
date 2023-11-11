ed793bef8c874a86cfad5e1d696015587f6662cd
Shreyan Mallik <shrnmallik@gmail.com>
2023-11-11 15:50:17 -0500

    feat: added app layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div id="container">
+  <div id="top-container">
+    <div id="logo" class="top-item">logo</div>
+    <div id="home-btn" class="top-item">home</div>
+    <div id="your-listings" class="top-item">listings</div>
+    <div id="account" class="top-item">account</div>
+  </div>
+  <div id="main-view">
+    <!-- <h1>HELLO WORLD!</h1> -->
+  </div>
+</div>
+
+<style>
+  @import "./styles.css";
+</style>
