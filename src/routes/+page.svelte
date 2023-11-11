57417b6ca77bc576b6da4f46a64d7deb0529a1ad
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-11-11 15:57:17 -0500

    for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,20 @@
 <script>
+    import App from "./App.svelte";
 </script>
 
 <div id="container">
-  <div id="top-container">
-    <div id="logo" class="top-item">logo</div>
-    <div id="home-btn" class="top-item">home</div>
-    <div id="your-listings" class="top-item">listings</div>
-    <div id="account" class="top-item">account</div>
-  </div>
-  <div id="main-view">
-    <!-- <h1>HELLO WORLD!</h1> -->
-  </div>
+    <div id="top-container">
+        <div id="logo" class="top-item">logo</div>
+        <div id="home-btn" class="top-item">home</div>
+        <div id="your-listings" class="top-item">listings</div>
+        <div id="account" class="top-item">account</div>
+    </div>
+    <div id="main-view">
+        <App />
+    </div>
 </div>
 
+
 <style>
-  @import "./styles.css";
+    @import "./styles.css";
 </style>
