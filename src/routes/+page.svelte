5f7581dd07a110a321c46b19de8f66965a70afed
Shreyan Mallik <shrnmallik@gmail.com>
2023-11-11 15:58:41 -0500

    fix: separated components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,3 @@
     import App from "./App.svelte";
 </script>
 
-<div id="container">
-    <div id="top-container">
-        <div id="logo" class="top-item">logo</div>
-        <div id="home-btn" class="top-item">home</div>
-        <div id="your-listings" class="top-item">listings</div>
-        <div id="account" class="top-item">account</div>
-    </div>
-    <div id="main-view">
-        <App />
-    </div>
-</div>
-
-
-<style>
-    @import "./styles.css";
-</style>
---
+++ src/routes/Main.svelte
@@ -0,0 +1,16 @@
+<div id="container">
+    <div id="top-container">
+        <div id="logo" class="top-item">logo</div>
+        <div id="home-btn" class="top-item">home</div>
+        <div id="your-listings" class="top-item">listings</div>
+        <div id="account" class="top-item">account</div>
+    </div>
+    <div id="main-view">
+        <App />
+    </div>
+</div>
+
+
+<style>
+    @import "./styles.css";
+</style>
