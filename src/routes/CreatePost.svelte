badc516dc45bfd0e6ec0ead9084ff8775d63d0b6
Shreyan Mallik <shrnmallik@gmail.com>
2023-11-11 20:11:21 -0500

    feat: added create post

--- src/routes/CreatePost.svelte
+++ src/routes/CreatePost.svelte
@@ -1,74 +1,77 @@
 <script>
+  import { makeAuthenticatedRequest } from "./auth/store.js";
   export let show = false;
-  let name, price, image, condition;
+  let name, price, image, condition, description;
 
   function close() {
     show = false;
   }
-  function handleSubmit() {}
+  async function handleSubmit(e) {
+    const formData = new FormData();
+    formData.append("product_name", name);
+    formData.append("condition", condition);
+    formData.append("price", price);
+    formData.append("description", description);
+    formData.append("image", image);
+    await makeAuthenticatedRequest("/create", formData);
+    close();
+  }
 </script>
 
 {#if show}
   <div class="modal">
     <div class="modal-content">
+      <h1 id="title">Create Post!</h1>
       <span class="close" on:click={close}>✕</span>
       <!-- Your modal content goes here -->
-      <form on:submit|preventDefault={handleSubmit}>
-        <input type="text" id="name" bind:value={name} placeholder="Product Name" required />
-        <br />
-        <br />
-        <input type="text" id="condition" bind:value={condition} placeholder="Condition" required />
-        <br />
-        <br />
-        <input type="number" id="price" bind:value={price} placeholder="Price" required />
-        <br />
-        <br />
-        <label for="image">Image: </label>
+      <form on:submit|preventDefault={handleSubmit} id="form">
+        <input
+          type="text"
+          id="name"
+          bind:value={name}
+          placeholder="Product Name"
+          class="form-item"
+          required
+        />
+        <input
+          type="text"
+          id="condition"
+          bind:value={condition}
+          placeholder="Condition"
+          class="form-item"
+          required
+        />
+        <input
+          type="number"
+          id="price"
+          bind:value={price}
+          placeholder="Price"
+          class="form-item"
+          required
+        />
+        <input
+          type="text"
+          id="description"
+          bind:value={description}
+          placeholder="Description"
+          class="form-item"
+          required
+        />
+        <label for="image" id="image-label">Image: </label>
         <input
           type="file"
           id="image"
           on:change={(e) => (image = e.target.files[0])}
           accept="image/*"
+          class="form-item"
           required
         />
-        <br />
-        <br />
-
-        <button type="submit">Submit</button>
+        <button type="submit" id="submit-btn">Submit</button>
       </form>
     </div>
   </div>
 {/if}
 
 <style>
-  /* Add your modal styles here */
-  .modal {
-    display: block;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    justify-content: center;
-    align-items: center;
-    z-index: 1;
-  }
-
-  .modal-content {
-    background: #fff;
-    padding: 20px;
-    border-radius: 8px;
-    position: relative;
-    max-width: 400px;
-    margin: 0 auto;
-  }
-
-  .close {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 20px;
-    cursor: pointer;
-  }
+  @import "./CreatePost.css";
 </style>
