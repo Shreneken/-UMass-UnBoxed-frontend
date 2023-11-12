28aba7d722bc36f89b7bdb017f8e9951a9cab712
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-11-11 19:45:42 -0500

    Good

--- src/routes/auth/store.js
+++ src/routes/auth/store.js
@@ -3,14 +3,29 @@ import { SERVER_URL } from "../utils/constants.js";
 
 export const user = writable(null);
 
-export async function makeAuthenticatedRequest(endpoint) {
+export async function makeAuthenticatedRequest(endpoint, reqBody) {
     const [uid, token] = [
         localStorage.getItem("uid"),
         localStorage.getItem("token"),
     ];
-    return fetch(`${SERVER_URL}/api/${endpoint}?uid=${uid}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    }).then((resp) => resp.json());
+
+    let response;
+    if (reqBody === undefined) {
+        response = fetch(`${SERVER_URL}/api/${endpoint}?uid=${uid}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    } else {
+        response = fetch(`${SERVER_URL}/api/${endpoint}?uid=${uid}`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(reqBody),
+        });
+    }
+
+    return response.json();
 }
